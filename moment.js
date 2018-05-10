93b0cf95aa68f822fcbc0861e8da64d614a0a005
lijiawei <ljw@Live.jp>
2018-05-10 15:03:55 +0800

    0.0.2 const,let => var

--- moment.js
+++ moment.js
@@ -9,9 +9,9 @@ export default {
         // Vue filter
         // https://github.com/saman/vue-moment-jalaali
         Vue.filter('moment', function () {
-            const args = Array.prototype.slice.call(arguments)
-            const input = args.shift()
-            let date
+            var args = Array.prototype.slice.call(arguments)
+            var input = args.shift()
+            var date
             if (Array.isArray(input) && typeof input[0] === 'string') {
                 // If input is array, assume we're being passed a format pattern to parse against.
                 // Format pattern will accept an array of potential formats to parse against.
@@ -29,18 +29,18 @@ export default {
             }
 
             function parse () {
-                const args = Array.prototype.slice.call(arguments)
-                const method = args.shift()
+                var args = Array.prototype.slice.call(arguments)
+                var method = args.shift()
                 switch (method) {
                     case 'add':
                         // Mutates the original moment by adding time.
                         // http://momentjs.com/docs/#/manipulating/add/
-                        const addends = args.shift()
+                        var addends = args.shift()
                             .split(',')
                             .map(Function.prototype.call, String.prototype.trim)
-                        const objects = {}
-                        for (let n = 0; n < addends.length; n++) {
-                            const addend = addends[n].split(' ')
+                        var objects = {}
+                        for (var n = 0; n < addends.length; n++) {
+                            var addend = addends[n].split(' ')
                             objects[addend[1]] = addend[0]
                         }
                         date = date.add(objects)
@@ -48,12 +48,12 @@ export default {
                     case 'subtract':
                         // Mutates the original moment by subtracting time.
                         // http://momentjs.com/docs/#/manipulating/subtract/
-                        const subtrahends = args.shift()
+                        var subtrahends = args.shift()
                             .split(',')
                             .map(Function.prototype.call, String.prototype.trim)
-                        const obj = {}
-                        for (let n = 0; n < subtrahends.length; n++) {
-                            const subtrahend = subtrahends[n].split(' ')
+                        var obj = {}
+                        for (var n = 0; n < subtrahends.length; n++) {
+                            var subtrahend = subtrahends[n].split(' ')
                             obj[subtrahend[1]] = subtrahend[0]
                         }
                         date = date.subtract(obj)
@@ -61,14 +61,14 @@ export default {
                     case 'from':
                         // Display a moment in relative time, either from now or from a specified date.
                         // http://momentjs.com/docs/#/displaying/fromnow/
-                        let from = 'now'
+                        var from = 'now'
                         if (args[0] === 'now') args.shift()
 
                         if (moment(args[0]).isValid()) {
                             // If valid, assume it is a date we want the output computed against.
                             from = moment(args.shift())
                         }
-                        let removeSuffix = false
+                        var removeSuffix = false
                         if (args[0] === true) {
                             args.shift()
                             removeSuffix = true
@@ -82,7 +82,7 @@ export default {
                     case 'calendar':
                         // Formats a date with different strings depending on how close to a certain date (today by default) the date is.
                         // http://momentjs.com/docs/#/displaying/calendar-time/
-                        let referenceTime = moment()
+                        var referenceTime = moment()
                         if (moment(args[0]).isValid()) {
                             // If valid, assume it is a date we want the output computed against.
                             referenceTime = moment(args.shift())
@@ -93,7 +93,7 @@ export default {
                         // Format
                         // Formats a date by taking a string of tokens and replacing them with their corresponding values.
                         // http://momentjs.com/docs/#/displaying/format/
-                        const format = method
+                        var format = method
                         date = date.format(format)
                 }
                 if (args.length) parse.apply(parse, args)
